5b90a2cbdb6acc6f8f7152f2577bab3d02300c2f
Anatoliy Serputov <aserputov@myseneca.ca>
2021-11-18 20:26:34 -0500

    Dist Folder changed due to new version of Node.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
 import fs from "fs";
+import path from "path";
 import { Options } from "./options.js";
 
-fs.rmdirSync("dist", { recursive: true });
+// fs.rmdirSync("dist", { recursive: true });
+const directory = "dist";
+
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
 Options();
